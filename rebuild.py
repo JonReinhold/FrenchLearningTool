c484326b36515fb6fb254c97638d8c0a0209bed3
Jon Reinhold <jon.reinhold15@gmail.com>
2017-08-03 21:54:11 -0500

    Create rebuild.py

---
+++ rebuild.py
@@ -0,0 +1,8 @@
+import random
+frenchDict = {'yes':'oui','no':'non','hello':'bonjour'}
+#for key, value in frenchDict.items():
+#    print(key)
+x = 0
+while x < 1:
+    index = random.randint(0,len(frenchDict))
+    for key in frenchDict.items()
