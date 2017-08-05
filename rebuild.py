02de8e87cb4ac2087b4f55df5ca0b82691dbff99
unknown <jon.reinhold15@gmail.com>
2017-08-05 11:15:20 -0500

    updated main loop:wq

--- rebuild.py
+++ rebuild.py
@@ -1,3 +1,4 @@
+#set up test list, will pull from file later
 import random
 frenchList = [('yes','oui'),('no','non'),('hello','bonjour')]
 
@@ -6,9 +7,13 @@ def wordFeed():
         global word
         word = frenchList[index]
         return word[1]
+print("Test your knowledge! Type Q to quit.")
+while True:
+    response = input("What is " + wordFeed() + " in English? ")
+    if response == word[0]:
+        print("Correct!")
+    elif response == "Q":
+        break
+    else:
+        print("Wrong!")
 
-response = input("What is " + wordFeed() + " in English? ")
-if response == word[0]:
-    print("Correct!")
-else:
-    print("Wrong!")
