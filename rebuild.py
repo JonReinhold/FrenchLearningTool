1ff04653164641456547e982e233c06ef1d6b216
unknown <jon.reinhold15@gmail.com>
2017-08-05 11:21:58 -0500

    added help page

--- rebuild.py
+++ rebuild.py
@@ -7,13 +7,15 @@ def wordFeed():
         global word
         word = frenchList[index]
         return word[1]
-print("Test your knowledge! Type Q to quit.")
+print("Test your knowledge! Type H for help or Q to quit. ")
 while True:
     response = input("What is " + wordFeed() + " in English? ")
     if response == word[0]:
         print("Correct!")
     elif response == "Q":
         break
+    elif response == "H":
+        print("//TODO add help text.")
     else:
         print("Wrong!")
 
