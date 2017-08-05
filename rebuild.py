709a852795e323197635e32c0a44cec50648dcd2
Jon Reinhold <jon.reinhold15@gmail.com>
2017-08-04 21:36:02 -0500

    Update rebuild.py

--- rebuild.py
+++ rebuild.py
@@ -2,7 +2,7 @@ import random
 frenchDict = {'yes':'oui','no':'non','hello':'bonjour'}
 #for key, value in frenchDict.items():
 #    print(key)
-x = 0
-while x < 1:
+x = True
+while x = True:
     index = random.randint(0,len(frenchDict))
     for key in frenchDict.items()
