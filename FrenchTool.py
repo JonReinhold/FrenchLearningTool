c1c70bcf3af62ecb09a2f4c734938b299c3eff2c
Jon Reinhold <jon.reinhold15@gmail.com>
2017-09-05 21:13:06 -0500

    Update FrenchTool.py

--- FrenchTool.py
+++ FrenchTool.py
@@ -1,6 +1,9 @@
 import random 
 from fuzzywuzzy import fuzz
-frenchList = [['yes','oui',0],['no','non',0],['hello','bonjour',0],['not','pas',0],['you','vous',0],['in','dans',0],['dictionary', 'dictionnaire', 0]]
+
+#or read from file with a more robust dictionary 
+frenchList = [['yes','oui',0],['no','non',0],['hello','bonjour',0],['not','pas',0],
+              ['you','vous',0],['in','dans',0],['dictionary', 'dictionnaire', 0]]
 orderedFrenchList = []
 
 def wordFeed():
